b3beac4e65265eb5146e3458be34a4bf39de0a58
Maria Colonna <mcolonna@pasteur.fr>
2026-01-27 11:14:16 +0100

    fix: Correct kfold training script syntax

--- scripts/kfold_train.sh
+++ scripts/kfold_train.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 for i in {0..4}
+do
 tsp python3 /opt/DeepSecE_for_ft/train.py --model effectortransformer --data_dir data/train_data_DeepsecE_ft --batch_size 32 --lr 5e-5 --weight_decay 4e-5 --dropout_rate 0.4 --num_layers 1 --num_heads 4 --max_epochs 160 --warm_epochs 1 --patience 5 --lr_scheduler cosine --lr_decay_steps 30 --kfold 5 --fold_num $i --log_dir runs/attempt_cv --model_initial ./apptainer/DeepSecE/checkpoint.pt
 done
