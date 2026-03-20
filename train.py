af7699328fa65098a5fb2ac151ae36a6d592a647
Maria Colonna <mcolonna@pasteur.fr>
2026-03-20 10:41:57 -0300

    feat: Save checkpoints at specific epoch intervals

--- train.py
+++ train.py
@@ -165,11 +165,10 @@ def main(args):
         for key, value in valid_metrics.items():
             writer.add_scalar('Valid/' + key, value, epoch+1)
 
-        #epochs_checkpoint = [40, 60, 80, args.max_epochs]
+        epochs_checkpoint = [10, 20, args.max_epochs//4, args.max_epochs//2, 3*args.max_epochs//4, args.max_epochs]
 
-        #if epoch + 1 in epochs_checkpoint:
-    
-    torch.save(model.state_dict(), os.path.join(log_dir, 'checkpoint.pt')) #incfold - saving checkpoint
+        if epoch + 1 in epochs_checkpoint:
+            torch.save(model.state_dict(), os.path.join(log_dir,f'{epoch+1}_checkpoint.pt')) #incfold - saving checkpoint
 
         #early_stopping(valid_f1, model)
         #if early_stopping.early_stop:
